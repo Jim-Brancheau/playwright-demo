a0ffd7f61a45a07c1efb8355692559792beba5d2
Jim Brancheau <jim.brancheau@gmail.com>
2022-05-26 23:15:30 -0400

    Remove tesults reporter, add junit results output, send to newrelic

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,8 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['playwright-tesults-reporter', {'tesults-target': process.env.TESULTS_TARGET,
-'tesults-build-name': process.env.RUN_ID }],['html']],
+  reporter: [['junit', { outputFile: 'junit/playwrightResults.xml'}],['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
